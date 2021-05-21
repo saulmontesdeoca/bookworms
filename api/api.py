1531abfb5469bf2260527161c20097230b4296b0
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-05-21 10:17:18 -0500

    Changing api response to application/json

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import Response
 from flask_pymongo import PyMongo
-
+from bson import json_util
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'bookworms'
@@ -10,11 +11,20 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/bookworms'
 
 mongo = PyMongo(app)
 
-@app.route('/findone/<id>', methods=['GET'])
-def hello_world(id):
+# Get a book by book_id
+@app.route('/find_book/<id>', methods=['GET'])
+def get_book(id):
     books = mongo.db.books
-    s = books.find_one({'book_id': id})
-    return jsonify({'result' : s['title']})
+    book = books.find_one({'book_id': id})
+    book = json_util.dumps(book)
+    return Response(book, mimetype='application/json')
 
+# Get an author by author_id
+@app.route('/find_author/<id>', methods=['GET'])
+def get_author(id):
+    authors = mongo.db.authors
+    author = authors.find_one({'author_id': id})
+    author = json_util.dumps(author)
+    return Response(author, mimetype='application/json')
 if __name__ == '__main__':
     app.run(debug=True)
