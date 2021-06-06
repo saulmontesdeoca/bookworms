c1d33bf5d5e061cb7c66f31f65608cbc7a40e91f
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-06-06 02:41:20 -0500

    🎪   Styling BookDetails page

---
+++ bookworms/src/components/BookInfoHeader/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Image, Row, Col } from 'react-bootstrap';
+
+const BookInfoHeader = (props) => {
+    return (
+        <div style={{position: 'relative', margin: 20, height: '14rem', overflow:'hidden', borderRadius: '30'}}>
+            <Image draggable={false} className='blur-intense' src={props.cover} fluid style={{height: '100%', width: '100%', objectFit: 'cover'}} rounded />
+            <Card style={{ 
+                    position: 'absolute', 
+                    width:'10%',
+                    height:'80%', 
+                    bottom: 0,
+                    top: 0, 
+                    alignSelf: 'center', 
+                    left: 0, 
+                    right: 0, 
+                    margin: 'auto',
+                    backgroundColor: 'rgba(255, 255, 255, 0)',
+                    borderColor: 'rgba(255, 255, 255, 0)'
+            }}>
+                <Card.Body style={{fontSize: 24, fontWeight: 'bolder', padding:0}}>
+                    <Image draggable={false} src={props.cover ? props.cover : 'https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png'} fluid style={{height: '100%', objectFit: 'cover'}} rounded />
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default BookInfoHeader;
--- bookworms/src/pages/BookDetails/index.js
+++ bookworms/src/pages/BookDetails/index.js
@@ -1,32 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import Layout from '../../components/Layout';
 import { Container, Row, Col } from 'react-bootstrap';
-import BookshelfCard from '../../components/BookshelfCard';
+import BookInfoHeader from '../../components/BookInfoHeader';
+import ReactStars from "react-rating-stars-component";
+
+import auth from '../../auth/Auth';
 
 const BookDetails = () => {
     const { id } = useParams();
     const [bookInfo, setBookInfo] = useState({});
+    const history = useHistory();
 
     useEffect(() => {
-        const fetchAuthors = (authors) =>{
-            let auths = []
-            for(let i =0; i< authors.length; i++){
-                fetch(`/find_author/${authors[i].author_id}`).then( res => {
-                    res.json().then( author => {
-                        auths.push(author.name)
-                    })
-                })
-            }
-            return auths
-        }
         async function fetchBookInfo(){
-            await fetch(`/find_book/${id}`).then( res => {
-            res.json().then(data => {
-                let auths = fetchAuthors(data.authors);
-                data.authors = auths;
-                setBookInfo(data)
-            })})
+            await fetch(`/find_book/${id}`, 
+            {
+                credentials: 'include'
+            }
+            ).then( res => {
+                if(res.status == 408){
+                auth.logout(() => {
+                    document.cookie = "token="
+                    })
+                    history.push('/login')
+                }
+                else{
+                    res.json().then(data => {
+                        setBookInfo(data)
+                    })
+                }
+            })
         }
         fetchBookInfo();
     }, [id]);
@@ -36,11 +40,24 @@ const BookDetails = () => {
             {
                 bookInfo &&
                     <>
+                        <div className='mx-5 px-5'>
+                            <BookInfoHeader cover={bookInfo.image_url} title={bookInfo.title} authors={bookInfo.authors} rating={bookInfo.average_rating}/>
+                        </div>
                         <Container>
                             <Row>
                                 <Col>
-                                    <BookshelfCard cover={bookInfo.image_url} title={bookInfo.title} authors={bookInfo.authors} rating={bookInfo.average_rating}/>
                                 </Col>
+                            </Row>
+                            <Row className='mt-3'>
+                                <h1>{bookInfo.title}</h1>
+                                <ReactStars
+                                    count={5}
+                                    value={bookInfo.average_rating}
+                                    isHalf= {true}
+                                    size={24}
+                                    activeColor="white"
+                                />
+                                <p>{bookInfo.description}</p>
                             </Row>
                         </Container>
                     </>
